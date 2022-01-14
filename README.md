5c42e07caeb81eb014e4cadbd17a26c6727de492
Cornelia Herman <corneliabherman@gmail.com>
2022-01-13 22:20:49 -0500

    updated all files but having issues getting it to start:dev. need to look at the scripts to make sure they are correct

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Text-Editor
 A text editor web app that allows you to manage text via any browser
+
+
+https://developer.mozilla.org/en-US/docs/Web/API/IDBCursor/update
+https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/put
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log('Put  to the database');
+  const jateDb = await openDB('content', 1);
+  const tx = jateDb.transaction('content', 'readwrite').objectStore('content');
+  const objectStoreContent = objectStore.get(content)
+  objectStoreContent.onsuccess = () => {
+    const data = objectStoreContent.result
+    const updateContentRequest = tx.put(data);
+    updateContentRequest.onerror = function (event) {
+      console.error('getDb not implemented');
+    };
+    updateContentRequest.onsuccess = function (event) {
+      displayData();
+    }
+  }
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('content', 1);
+  const tx = jateDb.transaction('content', 'readonly');
+  const store = tx.objectStore('content');
+  const request = store.getAll();
+  const result = await request;
+  request.onsuccess = function (event) {
+    return result;
+  };
+  request.onerror = function (event) {
+    console.error('getDb not implemented');
+  };
+};
 
 initdb();
