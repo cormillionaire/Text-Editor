cb18dd49535c2307490eaf2ae7f4954d6c1b04db
Cornelia Herman <corneliabherman@gmail.com>
2022-01-15 21:05:30 -0500

    fixed the index db - tried to pull an id that didnt exist so just defined it as one in accordance to the assets for this homework:

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 A text editor web app that allows you to manage text via any browser
 
 
+https://github.com/cormillionaire/Text-Editor
+
+https://jate-cormillionaire.herokuapp.com/
+
 https://developer.mozilla.org/en-US/docs/Web/API/IDBCursor/update
 https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/put
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite')
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, content: content });
+  const request = store.put({ id: 1, content: content });
   const result = await request;
   result.onsuccess = function (event) {
     console.log('Data saved to the database', result);
